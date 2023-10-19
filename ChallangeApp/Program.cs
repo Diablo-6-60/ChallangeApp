ea230c01074f2fc43ca5f2e7c97cd966be195af0
Diablo-6-60 <gutkowskakr@gmail.com>
2023-10-19 13:48:47 +0200

    Zadanie dzień 4

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+char gender = 'K';
+int Age1 = 33;
+int newAge = Age1 - 5;
+
+int Age2 = 17;
+char genger2 = 'M';
+
+Console.WriteLine(newAge);
+
+if (newAge > 33);
+var isValid = 28 > 33;
+
+Console.WriteLine(isValid);
+
+if (name == "Ewa" && Age1 == 33) ;
+
+Console.WriteLine("Ewa lat 33");
+
+if (newAge == 28) ;
+
+Console.WriteLine(" Kobieta poniżej 30 lat");
+
+if (Age2 < 18 && genger2 == 'M');
+
+Console.WriteLine(" Niepełnoletni mężczyzna");
